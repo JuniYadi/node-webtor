dcefabe5636e4dd82a934ba07c77f361bd1b66af
JuniYadi <juniyadi94@gmail.com>
2020-02-25 05:26:52 +0700

    Added Custom Files Torrent

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const WebTorrent = require('webtorrent')
 
 const client = new WebTorrent()
 
-const torrentId = 'magnet:?xt=urn:btih:44YQRS6WFD7OLTZAHLG7M2GFX5C5A6AQ&dn=ubuntu-18.04.4-live-server-amd64.iso&xl=912261120&tr=https%3A%2F%2Ftorrent.ubuntu.com%2Fannounce'
-
+const torrentId = process.env.FILE_TOR
 const path = process.env.PATH_TOR
 
 client.add(torrentId, { path: path }, (torrent) => {
