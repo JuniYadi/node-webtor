67c7fb8c42d8bae344c7ab67e1498fa982716574
JuniYadi <juniyadi94@gmail.com>
2020-02-25 05:21:03 +0700

    Added Custom Path

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const client = new WebTorrent()
 
 const torrentId = 'magnet:?xt=urn:btih:44YQRS6WFD7OLTZAHLG7M2GFX5C5A6AQ&dn=ubuntu-18.04.4-live-server-amd64.iso&xl=912261120&tr=https%3A%2F%2Ftorrent.ubuntu.com%2Fannounce'
 
-client.add(torrentId, { path: '/home/yadi/gd-dl' }, (torrent) => {
+const path = process.env.PATH_TOR
+
+client.add(torrentId, { path: path }, (torrent) => {
     torrent.on('done', function () {
         console.log('torrent download finished')
     })
